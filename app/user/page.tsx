60c103f0c8e72bb8678b51580567506f44ca9465
Godwin Opara <godwinopara62@yahoo.com>
2024-01-05 23:33:25 +0100

    updated metadata

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,21 +1,12 @@
-"use client";
-
 import Dashboard from "@/components/Dashboard/Dashboard";
-import { useUserContext } from "@/hooks/useUserContext";
-import { Metadata } from "next";
-import { useRouter } from "next/navigation";
-import { useEffect, useLayoutEffect } from "react";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-	title: "Universal Cryptosphere Trade | Dashboard",
-	description: "This is Dashboard page for Universal Cryptosphere Users",
-	// other metadata
-};
+// export const metadata: Metadata = {
+// 	title: "Universal Cryptosphere Trade | Dashboard",
+// 	description: "This is Dashboard page for Universal Cryptosphere Users",
+// };
 
 export default function Home() {
-	// const { user } = useUserContext();
-	// const router = useRouter();
-
 	return (
 		<>
 			<Dashboard />
